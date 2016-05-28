eaf96d9930adba5934c9759ee43600519038ed2f
Thomsen Cummings <thomsencummings@gmail.com>
2016-05-28 13:47:30 -0400

    api controllers, node modules, frontend entrypoind

---
+++ frontend/okCupidClone.jsx
@@ -0,0 +1,9 @@
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(
+    <div>Walcome to wobsite</div>,
+    document.getElementById('content')
+  );
+});
