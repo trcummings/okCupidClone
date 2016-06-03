662d57cca687340eb25ffd8985253c0c24f07bc5
App Academy Student <appacademy@Apps-Mac-mini-83.local>
2016-06-03 09:54:35 -0400

    ghh

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,35 +2,34 @@
 #
 # Table name: messages
 #
-#  id         :integer          not null, primary key
-#  user_1_id  :integer          not null
-#  user_2_id  :integer          not null
-#  content    :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  sender_id   :integer          not null
+#  receiver_id :integer          not null
+#  content     :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 
 class Message < ActiveRecord::Base
-  validates :user_1_id, :user_2_id, :content
 
-  belongs_to(
-    :sender,
-    class_name: 'User',
-    foreign_key: :user_1_id,
-    primary_key: :id
-  )
-
-  belongs_to(
-    :receiver,
-    class_name: "User",
-    foreign_key: :user_2_id,
-    primary_key: :id
-  )
-
-  def self.chat_history(user1id, user2id)
-    Message
-      .select(:content)
-      .where("(user_1_id = ? AND user_2_id = ? ) OR (user_2_id = ? AND user_1_id = ?)", user1id, user2id)
-      .uniq
-  end
+  # belongs_to(
+  #   :sender,
+  #   class_name: 'User',
+  #   foreign_key: :user_1_id,
+  #   primary_key: :id
+  # )
+  #
+  # belongs_to(
+  #   :receiver,
+  #   class_name: "User",
+  #   foreign_key: :user_2_id,
+  #   primary_key: :id
+  # )
+  #
+  # def self.chat_history(user1id, user2id)
+  #   Message
+  #     .select(:content)
+  #     .where("(user_1_id = ? AND user_2_id = ? ) OR (user_2_id = ? AND user_1_id = ?)", user1id, user2id)
+  #     .uniq
+  # end
 end
