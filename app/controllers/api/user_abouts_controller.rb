749526a4b1c6f672dbe2765bd3c5ec05d63202b4
App Academy Student <appacademy@Apps-Mac-mini-83.local>
2016-06-06 18:28:32 -0400

    finished about edit form

--- app/controllers/api/user_abouts_controller.rb
+++ app/controllers/api/user_abouts_controller.rb
@@ -7,8 +7,21 @@ class Api::UserAboutsController < ApplicationController
 
   def update
     @user_about = UserAbout.find_by(user_id: current_user.id)
-    @user_about.update(params)
+    @user_about.update(about_params)
 
     render '/api/user_abouts/show'
+  end
+
+  def about_params
+    params.require(:about).permit(
+      :self_summary,
+      :doing_with_life,
+      :really_good_at,
+      :favorite_things,
+      :six_things,
+      :thinking_about,
+      :typical_friday,
+      :message_me_if
+    )
   end
 end
