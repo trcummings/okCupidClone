6329a562375c5b696bcb59ba34a966c27c9721cc
Thomsen Cummings <thomsencummings@gmail.com>
2016-06-07 19:03:16 -0400

    questions fully styled, responsive, and answers ready on front end, ready to add model

--- frontend/components/likes/whoLikesYou.jsx
+++ frontend/components/likes/whoLikesYou.jsx
@@ -3,6 +3,9 @@ var PropTypes = React.PropTypes;
 var SessionStore = require('../../stores/sessionStore');
 
 var WhoLikesYou = React.createClass({
+  componentDidMount: function () {
+  },
+
   renderUserList: function () {
     var currentUser = SessionStore.currentUser();
 
@@ -11,8 +14,9 @@ var WhoLikesYou = React.createClass({
       {
         currentUser.likers.map(function (user, index) {
           return (
-            <li>
-              <span key={index}>{user.username}</span>
+            <li className='likelist-user' key={index}>
+              <img src={user.photo_url} />
+              <span>{user.username}</span>
             </li>
           );
         })
@@ -22,16 +26,27 @@ var WhoLikesYou = React.createClass({
   },
 
   render: function() {
+    var currentUser = SessionStore.currentUser();
 
     return (
       <div className='user-list-box'>
-        <ul>
+        <ul className='user-list-section-left'>
           {this.renderUserList()}
         </ul>
+
+        <section className='user-list-like-count'>
+          <i className='fa fa-star fa-2' aria-hidden='true'></i>
+          <h2>{currentUser.likers.length + ' people like you'}</h2>
+          <i className="fa fa-arrow-right fa-2" aria-hidden="true"></i>
+        </section>
+
+        <section className='user-list-about'>
+          <h1>About Who Likes You</h1>
+          <p>These are people who like you on PerfectPair’s site. Maybe they could like you in real life too! You don't get that kind of vulnerability every day. Imagine how messed up it would be if people just wore their insecurities on their chests. I know I wouldn't leave the house if every shirt I owned said "sensitive about the size of his hands"</p>
+        </section>
       </div>
     );
   }
-
 });
 
 module.exports = WhoLikesYou;
