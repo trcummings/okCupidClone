b9daf5b2391b7a94860fc755769ab12c1a20c72a
Thomsen Cummings <thomsencummings@gmail.com>
2016-06-07 22:02:26 -0400

    fixed mutual likes in session store, works now

--- frontend/stores/sessionStore.js
+++ frontend/stores/sessionStore.js
@@ -31,11 +31,14 @@ SessionStore.__onDispatch = function (payload) {
       break;
     case SessionConstants.ADD_USER_TO_LIKES:
       _current_user.likees.push(payload.otherUser);
+      _current_user.mutualLikes.push(payload.otherUser);
       this.__emitChange();
       break;
     case SessionConstants.REMOVE_USER_FROM_LIKES:
       var removalIndex = _current_user.likees.indexOf(payload.otherUser);
+      var mutualRemovalIndex = _current_user.mutualLikes.indexOf(payload.otherUser);
       _current_user.likees.splice(removalIndex, 1);
+      _current_user.mutualLikes.splice(mutualRemovalIndex, 1);
       this.__emitChange();
       break;
     case SessionConstants.ADD_NEW_ANSWER_TO_USER:
